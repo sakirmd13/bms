9ddf4098a6c1c48bf79fca3833acdbe4fbce7c61
sakirmd13 <mds079475@gmail.com>
2024-11-11 17:03:08 +0530

    1st commit

---
+++ revers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[20];
+    int i, length;
+    printf("Enter the string:\n");
+    gets(str);
+    length = strlen(str);
+    printf("reverse string: \n");
+    for (i = length; i >= 0; i--)
+    {
+        printf("%c", str[i]);
+    }
+}
